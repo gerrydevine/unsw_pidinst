82b4a32b7289cc6de65e2c59bfe9c59a1f4bf833
Gerry Devine <gerry.devine@unsw.edu.au>
2025-01-14 14:40:40 +1100

    removing redundant .gitignore

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='unsw_pidinst',
     version='1.0',
-    packages=find_packages(include=['instrument']),
+    packages=find_packages(),
     install_requires = []
 )
